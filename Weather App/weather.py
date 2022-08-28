1d9ebd932b55a8340ec22fc912d30198e00904f5
Daniel Qiu <yqiu6302@uni.sydney.edu.au>
2022-08-28 11:16:31 +1000

    weather app

---
+++ Weather App/weather.py
@@ -0,0 +1,22 @@
+import requests
+from flask import Flask, render_template
+
+def get_weather_results():
+    api_url = "https://api.openweathermap.org/data/2.5/weather?q=Sydney,AU&units=metric&appid=a4aebd2f559f59815f2f5717e38eb312"
+    r = requests.get(api_url)
+    return r.json()
+
+app = Flask(__name__)
+@app.route('/')
+def weather_dashboard():
+    data = get_weather_results()
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('home.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
+
+if __name__ =='__main__':
+    app.run()
+
